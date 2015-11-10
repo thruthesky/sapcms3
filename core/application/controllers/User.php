efdc220c68651a91d83bfb234848a953d02aaccb
thruthesky <thruthesky@gmail.com>
2015-11-10 18:21:11 +0900

    refactoring

---
+++ core/application/controllers/User.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    public function register()
+    {
+        echo 'User Registration';
+    }
+    public function page()
+    {
+        echo 'User Page';
+    }
+}
