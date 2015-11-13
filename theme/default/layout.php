717fd4ba54ffe514c4644274542418cb8f6db220
thruthesky <thruthesky@gmail.com>
2015-11-13 21:03:18 +0900

    Updating basic

--- theme/default/layout.php
+++ theme/default/layout.php
@@ -1,11 +1,12 @@
 <?php
 
+
 ?><!doctype html>
 <html>
 <head>
     <title><?php echo $this->data['title']?></title>
 </head>
 <body>
-<?php include "templates/{$this->data['page']}.php"?>
+<?php include "page/{$this->data['page']}.php"?>
 </body>
 </html>
