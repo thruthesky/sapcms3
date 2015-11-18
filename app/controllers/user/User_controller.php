63a43b10096603c0e7317b818979adfdc23b0a36
thruthesky <thruthesky@gmail.com>
2015-11-18 22:45:08 +0900

    Code factoring  - Unit test  - User interface

--- app/controllers/user/User_controller.php
+++ app/controllers/user/User_controller.php
@@ -8,6 +8,24 @@ class User_controller extends MY_Controller {
             'page' => 'user.register',
         ];
 
+        $this->form_validation->set_rules(
+            'email', 'Email',
+            'trim|required|min_length[10]|max_length[64]|valid_email|is_unique[user.email]',
+            array(
+                'required'      => 'Please input your email address.',
+                'is_unique'     => 'Email %s already exists.'
+            )
+        );
+
+
+        $this->form_validation->set_rules('password', 'Password', 'required',
+            array('required' => 'You must provide a %s.')
+        );
+        $this->form_validation->set_rules('password_confirm', 'Password Confirmation', 'trim|required|matches[password]');
+
+        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required');
+        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required');
+
         if ($this->form_validation->run() == FALSE)
         {
         }
--- theme/default/page/user.register.php
+++ theme/default/page/user.register.php
@@ -2,17 +2,29 @@
 <?php echo validation_errors(); ?>
 <?php echo form_open('/user/register'); ?>
 
-<h5>Username</h5>
-<input type="text" name="username" value="" size="50" />
+<h5>Email Address</h5>
+<input type="text" name="email" value="<?php echo set_value('email')?>" size="50" />
 
 <h5>Password</h5>
-<input type="text" name="password" value="" size="50" />
+<input type="text" name="password" value="<?php echo set_value('password')?>" size="50" />
 
 <h5>Password Confirm</h5>
-<input type="text" name="passconf" value="" size="50" />
+<input type="text" name="password_confirm" value="<?php echo set_value('password_confirm')?>" size="50" />
 
-<h5>Email Address</h5>
-<input type="text" name="email" value="" size="50" />
+<h5>First Name</h5>
+<input type="text" name="first_name" value="<?php echo set_value('first_name')?>" size="50" />
+
+<h5>Middle Name</h5>
+<input type="text" name="middle_name" value="<?php echo set_value('middle_name')?>" size="50" />
+
+<h5>Last Name</h5>
+<input type="text" name="last_name" value="<?php echo set_value('last_name')?>" size="50" />
+
+<h5>Address</h5>
+<input type="text" name="address" value="<?php echo set_value('address')?>" size="50" />
+
+
+
 
 <div><input type="submit" value="Submit" /></div>
 
