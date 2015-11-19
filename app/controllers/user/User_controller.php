0c48f3143900b1a749cfd057652ea4a9982d2336
thruthesky <thruthesky@gmail.com>
2015-11-19 19:50:04 +0900

    Code Factoring for Sapcms3

--- app/controllers/user/User_controller.php
+++ app/controllers/user/User_controller.php
@@ -32,46 +32,7 @@ class User_controller extends MY_Controller {
         }
         $this->render( $data );
     }
-    public function show() {
-        $m = entity('meta');
-        $m->set('c', 'd');
-        print_r($m);
-        echo '<hr>';
-
-        $t = entity('test')
-            ->set('t', 'test')
-            ->set('j', 'jennifer');
-        print_r($t);
-        echo '<hr>';
-
-        $m->set('j', 'jaeho song');
-        print_r($m);
-        echo '<hr>';
-
-        $e = entity('edu');
-        $e->set('e', 'education');
-        $e->set('d', 'decipline');
-        print_r($e);
-        echo '<hr>';
 
 
-        $m->set('name', 'SJ');
-        print_r($m);
-        echo '<hr>';
-
-        $another = entity('test');
-        $another->set('field', "This should be in another object");
-        print_r($another);
-        echo '<hr>';
-
-        $t->set('AAA', '1234')
-            ->set('BBB', '5555');
-        print_r($t);
-        echo '<hr>';
-    }
-
-    public function create() {
-
-    }
 }
 
