4765229a3bd87f89299f6815fb94356d1ef182af
thruthesky <thruthesky@gmail.com>
2015-11-20 16:02:03 +0900

    Code Factoring for Sapcms3 - Unit testing..

--- app/controllers/test/Test_controller.php
+++ app/controllers/test/Test_controller.php
@@ -18,11 +18,29 @@ class Test_controller extends MY_Controller
             }
         }
 
+        $errors = [];
         foreach( $this->unit->result() as $row ) {
-            if ( $row['Result'] == 'Passed' ) echo 'O';
-            else echo "<b style='color:red;'>X</b>";
+            if ( $row['Result'] == 'Passed' ) echo 'O ';
+            else {
+                echo "<b style='color:red;'>X</b> ";
+                $errors[] = $row;
+            }
         }
 
-        echo $this->unit->report();
+        if ( $errors ) {
+            echo "<h4>TEST ERROR</h4>";
+            foreach( $errors as $e ) {
+                echo "
+<pre>
+Test Name : {$e['Test Name']}
+File Name : {$e['File Name']}
+Line Number : {$e['Line Number']}
+                </pre>
+                <hr>
+                ";
+            }
+        }
+
+        // echo $this->unit->report();
     }
 }
