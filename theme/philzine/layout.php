75692de6848d5ff5b61de0eabc5b7b306aba8292
ontuebenjamin <ontuebenjamin@gmail.com>
2015-12-09 21:36:17 +0800

    using bootstrap 4

--- theme/philzine/layout.php
+++ theme/philzine/layout.php
@@ -10,8 +10,7 @@
     <?php echo theme_css('component')?>
     <?php echo theme_css('component.header')?>
     <?php echo theme_css('component.slide_menu')?>
-    <?php echo theme_css('component.footer')?>
-    <?php echo theme_css('component.grid')?>
+    <?php echo theme_css('component.footer')?>    
     <?php echo theme_css('component.front')?>
     <?php echo theme_css('state')?>
     <?php echo theme_css('theme')?>
@@ -21,17 +20,15 @@
     </script>
 </head>
 <body>
-<div class="layout">
-    <div class="header"><div class="header-inner"><?php include theme_script('header') ?></div></div>
-    <div class="slide-menu"><?php include 'page/menu.slide.php';?></div>
-    <div class="content">
-        <div class="content-inner">
-            <div class="error"><?php widget('error') ?></div>
-            <div class="page <?php echo $model?>"><?php include $path_theme_script ?></div>            
-        </div>
-    </div>
-    <div class="footer"><div class="footer-inner"><?php include theme_script('footer') ?></div></div>
+<div class="header">
+	<?php include theme_script('header') ?>
 </div>
+<div class="container">    
+	<div class="slide-menu"><?php include 'page/menu.slide.php';?></div>
+	<div class="error"><?php widget('error') ?></div>
+	<div class="page <?php echo $model?>"><?php include $path_theme_script ?></div>
+</div>
+<div class="footer"><div class="container"><?php include theme_script('footer') ?></div></div>
 <?php echo bootstrap_js()?>
 <?php echo js('/etc/js/common')?>
 <?php echo theme_js('default')?>
--- theme/philzine/page/front.monday.php
+++ theme/philzine/page/front.monday.php
@@ -1,44 +1,51 @@
-<div class='daily-front-page monday'>
-	<div class='grid3'>
-		<div class='a'>
-		<?php
-			$data = [];
-			
-			$images = data()->rows("model='post' AND id_entity <> 0 LIMIT 1");
-			if( !empty( $images ) ){
-				foreach( $images as $image ){
-					$data['images'][] = data()->load( $image['id'] );		
-				}
-						
-				widget( "philzine_hover_image_with_title", $data ); 
-			}
-			else{
-		?>
-			<h1>Empty Data</h1>
-		<?php	
-			}
-			
-			$bullet_posts = post_data()->rows("id <> 0 LIMIT 3");
-			widget( "philzine_bullet_list", $bullet_posts ); 
-		?>
-		</div>
-		<div class='b'>
-			<?php 
-				if( !empty( $data ) ) widget( "philzine_hover_image_with_title", $data );
-				else echo "<h1>Empty Data</h1>";
-				
-				$bullet_posts = post_data()->rows("id <> 0 LIMIT 3");
-				widget( "philzine_bullet_list", $bullet_posts ); 
-			?>
-		</div>
-		<div class='c'>
-			<?php 
-				if( !empty( $data ) ) widget( "philzine_hover_image_with_title", $data );
-				else echo "<h1>Empty Data</h1>";
-				
-				$bullet_posts = post_data()->rows("id <> 0 LIMIT 3");
-				widget( "philzine_bullet_list", $bullet_posts ); 
-			?>
-		</div>
-	</div>
+<?php
+	$images = data()->rows("model='post' AND id_entity <> 0 LIMIT 1");
+	if( !empty( $images ) ){
+		foreach( $images as $image ){
+			$data['images'][] = data()->load( $image['id'] );		
+		}						
+	}
+	
+	$active = null;
+?>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<?php if( count( $images ) > 1 ) { ?>
+  <ol class="carousel-indicators">
+	<?php for( $i = 0; $i < count($images); $i ++ ) { 
+		if( $i == 0 ) $carousel_active = "class='active'";
+		else $carousel_active = null;
+	?>
+    <li data-target="#carousel-example-generic" data-slide-to="0" <?php echo $carousel_active; ?>></li>
+    <?php } ?>
+  </ol>
+<?php } ?>
+  <div class="carousel-inner" role="listbox">
+	<?php foreach( $data['images'] as $im ){ 
+		$url = $im->get("url");
+		$title = post_data()->load( $im->get('id_entity') )->get('content');
+		if( empty( $active ) ) $is_active = " active";
+		else $is_active = null;
+	?>
+    <div class="carousel-item<?php echo $is_active; ?>">
+      <img style='width:100%;height:500px;' src="<?php echo $url; ?>" alt="<?php echo $title; ?>">
+	  <div class="carousel-caption">
+		<h3><?php echo $title ?></h3>		
+	  </div>
+    </div>
+	<?php
+		$active = true;
+		}		
+	?>
+  </div>
+<?php if( count( $images ) > 1 ) { ?>
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="icon-prev" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="icon-next" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+<?php } ?>
 </div>
