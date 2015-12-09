4de5fb5a872846f725be48cc834c7a6f7ad24add
ontuebenjamin <ontuebenjamin@gmail.com>
2015-12-09 22:02:01 +0800

    theme updates

--- theme/philzine/page/front.monday.php
+++ theme/philzine/page/front.monday.php
@@ -10,16 +10,6 @@
 ?>
 
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-<?php if( count( $images ) > 1 ) { ?>
-  <ol class="carousel-indicators">
-	<?php for( $i = 0; $i < count($images); $i ++ ) { 
-		if( $i == 0 ) $carousel_active = "class='active'";
-		else $carousel_active = null;
-	?>
-    <li data-target="#carousel-example-generic" data-slide-to="0" <?php echo $carousel_active; ?>></li>
-    <?php } ?>
-  </ol>
-<?php } ?>
   <div class="carousel-inner" role="listbox">
 	<?php foreach( $data['images'] as $im ){ 
 		$url = $im->get("url");
@@ -27,25 +17,22 @@
 		if( empty( $active ) ) $is_active = " active";
 		else $is_active = null;
 	?>
-    <div class="carousel-item<?php echo $is_active; ?>">
-      <img style='width:100%;height:500px;' src="<?php echo $url; ?>" alt="<?php echo $title; ?>">
+    <div class="carousel-item active">
+      <img style='width:100%;' src="<?php echo $url; ?>" alt="<?php echo $title; ?>">
 	  <div class="carousel-caption">
 		<h3><?php echo $title ?></h3>		
 	  </div>
     </div>
-	<?php
-		$active = true;
+	<?php	
 		}		
 	?>
   </div>
-<?php if( count( $images ) > 1 ) { ?>
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="icon-prev" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="icon-next" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-<?php } ?>
 </div>
+
+<ul class="list-group">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
